489e58bf83f65b7238d176f4b8453141c9d97852
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-23 19:45:12 +0100

    creating home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
+import NavBar from './Components/NavBar'
+import HeroSection from './Components/HeroSection'
+import HomeCards from './Components/HomeCards'
 
 const App = () => {
-  const name = ' My name is Zakaria '
-  const age = 'I am 25 years old'
-  const Friends = ['ahmed', 'zakaria', 'kilozerko', 'kim']
-  const joined = true;
   return (
-    <>
-      <div className='text-red font-bold text-4xl p-6 text-center'>
-        hello {name} , {age}.
-      </div>
-      <p className='font-bold p-8'>
-        {
-          Friends.map((Friends, index)=>(
-            <li key={index}>
-              {Friends}
-            </li>
-          ))
-        }
-      </p>
-      {
-        joined ? <button className='bg-green-500 p-4 rounded ml-4'>Click Here</button> : <button className='bg-amber-600 p-4 rounded ml-4'>You are imposter </button>
-      }
-      
-    </>
+    <div>
+      <NavBar/>
+      <HeroSection/>
+      <HomeCards/>
+    </div>
   )
 }
 
