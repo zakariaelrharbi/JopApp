ec865e6bef3a59e233d6f037656b5f3224505a9d
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-27 19:34:10 +0100

    adding path to link

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -29,7 +29,7 @@ const Joblisting = () => {
                         {job.location}
                       </div>
                       <a
-                        href={job.link}
+                        href={`/jobs/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                       >
                         Read More
