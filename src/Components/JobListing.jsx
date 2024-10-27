1c276396bbacf75ba31d043d8accac4c28f0cd61
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-27 19:43:49 +0100

    add key to job listing

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -13,7 +13,7 @@ const Joblisting = () => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {
               Jobs.map((job) => (
-                <JobListing1 job={job}/>
+                <JobListing1 key={job.id} job={job}/>
               ))
             }
           </div>
