de6f3acd588c5686565a07c3735115bad3566e14
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-28 19:38:20 +0100

    button view all jobs components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar'
 import HeroSection from './Components/HeroSection'
 import HomeCards from './Components/HomeCards'
 import Joblisting from './Components/Joblisting'
+import ViewAllJobs from './Components/ViewAllJobs'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <HeroSection/>
       <HomeCards/>
       <Joblisting/>
+      <ViewAllJobs/>
     </div>
   )
 }
