185cdb57a80c1873085fc226b420137b254e05a3
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-28 20:50:53 +0100

    adding localisation icon

--- src/Components/JobListing1.jsx
+++ src/Components/JobListing1.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { FaLocationDot } from "react-icons/fa6";
+
 
 const JobListing1 = ({job}) => {
   const [showMore, setShowMore] = useState(0);
@@ -29,7 +31,7 @@ const JobListing1 = ({job}) => {
                     <div className="border border-gray-100 mb-5" />
                     <div className="flex flex-col lg:flex-row justify-between mb-4">
                       <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg" />
+                        <FaLocationDot  className="inline text-lg mb-1 mr-1" />
                         {job.location}
                       </div>
                       <a
