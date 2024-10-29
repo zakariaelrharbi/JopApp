bead25019e7bede4aa115930ead568a2f762582b
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-29 23:35:32 +0100

    homepage done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection'
+import HomeCards from '../Components/HomeCards'
+import Joblisting from '../Components/Joblisting'
+import ViewAllJobs from '../Components/ViewAllJobs'
+
 
 const HomePage = () => {
   return (
     <>
-     <HeroSection/> 
+     <HeroSection/>
+     <HomeCards/>
+     <Joblisting/>
+     <ViewAllJobs/>
+      
     </>
   )
 }
