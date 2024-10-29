652b7c14c2858486c754936d9ea8f00ca9976d0e
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-29 23:31:20 +0100

    navbar layout added

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import {
 } from 'react-router-dom'
 import React from 'react'
 import HomePage from './Pages/HomePage'
+import MainLayout from './Layouts/MainLayout'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<HomePage/>}/>
+        <Route path='/' element={<MainLayout/>}>
+          <Route index element={<HomePage/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
