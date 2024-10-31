899d3c33185c14931be9de781c1d2bf1facb7221
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-31 21:00:08 +0100

    adding links to pages

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -28,12 +28,12 @@ const NavBar = () => {
                     >
                     Jobs
                     </Link>
-                    <a
-                    href="/add-job.html"
+                    <Link
+                    to="/add-job"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                     >
                     Add Job
-                    </a>
+                    </Link>
                 </div>
                 </div>
             </div>
