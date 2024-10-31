f0da225dbd292ff59917d1ef9b10cc609682105a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-10-31 21:13:42 +0100

    adding links to pages

--- src/Components/ViewAllJobs.jsx
+++ src/Components/ViewAllJobs.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ViewAllJobs = () => {
   return (
     <div>
       <section className="m-auto max-w-lg my-10 px-6">
-        <a
-            href="/jobs"
+        <Link
+            to="/JobsPage"
             className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
         >
             View All Jobs
-        </a>
+        </Link>
         </section>
 
     </div>
